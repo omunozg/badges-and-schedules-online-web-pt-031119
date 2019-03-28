a821afc15c6c17af340e0671448ff6ab27e30bf7
Oscar Munoz <omunozg@emory.edu>
2019-03-28 20:28:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(ppl)
+  ppl.map do |ppl|
+    "Hello, my name is #{ppl}."
+  end
+end
+
+def assign_rooms(ppl)
+  ppl.each_with_index.map do |ppl, index|
+    "Hello, #{ppl}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+
+def printer(ppl)
+  batch_badge_creator(ppl).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(ppl).each do |assignment|
+    puts assignment
+  end
+end
+  
