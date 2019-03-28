012852bd65e63b8c27b059ffb9d76f239858862d
Oscar Munoz <omunozg@emory.edu>
2019-03-28 19:52:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,11 @@ def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
 
-batch_badge_creator(namesarray)
+def batch_badge_creator(namesarray)
+  new_array = []
+  batch_badge_creator.each do |i|
+    badge_maker(i) << new_array
+  end
+  return new_array
+end
+
