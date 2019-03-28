cc2204e18f753e084ed013222c80b2b87f73aace
Oscar Munoz <omunozg@emory.edu>
2019-03-28 20:27:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,13 +3,13 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(ppl)
-  attendees.map do |ppl|
+  ppl.map do |ppl|
     "Hello, my name is #{ppl}."
   end
 end
 
 def assign_rooms(ppl)
-  attendees.each_with_index.map do |ppl, index|
+  ppl.each_with_index.map do |ppl, index|
     "Hello, #{ppl}! You'll be assigned to room #{index+1}!"
   end
 end
